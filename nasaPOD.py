c8a7dbc0a73d1ed636c1a6691c5055b542ad11e1
jacobalcu <101678531+jacobalcu@users.noreply.github.com>
2022-11-16 10:53:37 -0500

    Add files via upload

---
+++ nasaPOD.py
@@ -0,0 +1,27 @@
+import requests
+import json
+# import webbrowser
+
+def nasaPOD(string date):
+	#API KEY
+	API_KEY = 'E24iDq9jhvLGggYsFugUinAXI2aAeropVkq8DNZz'
+
+	#API URL
+	url='https://api.nasa.gov/planetary/apod'
+
+	#Parameters
+	params={
+		'api_key' : API_KEY,
+		'hd' : 'True',
+		#'date' : '1997-03-22',
+		'date': date
+	}
+
+	response = requests.get(url,params=params)
+	json_data = json.loads(response.text)
+	print(json_data)
+
+	image_url= json_data['hdurl']
+	# webbrowser.open(image_url)
+
+	return image_url
