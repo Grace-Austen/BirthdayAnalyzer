07dab81b21a593f0239076f469f1d8f2a433a559
Ferrin <fthreatt19@gmail.com>
2022-12-02 12:00:13 -0500

    updating billboard file (for real)

--- Billboard.js
+++ Billboard.js
@@ -1,49 +1,17 @@
-const { getChart } = require('billboard-top-100');
+const billboard = require("billboard-top-100").getChart;
 
-// date format YYYY-MM-DD
-getChart('hot-100', '2022-11-01', (err, chart) => {
-  if (err) console.log(err);
-  
-  console.log(chart.week);
- 
-  console.log(chart.previousWeek.date);
-  
-  console.log(chart.nextWeek.url);
+//Displays the top 100 songs
+function getSongs(callback) {
+	billboard('hot-100', function(err, songs) {
+	    if (err) console.log(err);
+	    let result = [];
+	    for(let i=0; i<songs.length; i++) {
+	    	let song = {name:songs[i].title, artist:songs[i].artist};
+	    	result.push(song);
+	    }
+	    callback(result);
+	    return;
+	});
+}
 
-  console.log(chart.nextWeek.date);
-  
-  console.log(chart.songs);
-  
-  console.log(chart.songs[3]);
-  
-  console.log(chart.songs[0].title);
-  
-  console.log(chart.songs[0].artist);
-  
-  console.log(chart.songs[0].rank);
- 
-  console.log(chart.songs[0].cover);
-  
-  console.log(chart.songs[0].position.positionLastWeek);
-  console.log(chart.songs[0].position.peakPosition);
-  console.log(chart.songs[0].position.weeksOnChart);
-});
-
-
-getChart((err, chart) => {
-  if (err) console.log(err);
-  console.log(chart);
-});
-
-getChart('rock-digital-song-sales', (err, chart) => {
-  if (err) console.log(err);
-  console.log(chart);
-});
-
-const { listCharts } = require('billboard-top-100');
-
-listCharts((err, charts) => {
-  if (err) console.log(err);
-  // array of all charts
-  console.log(charts);
-});
+module.exports = { getSongs, getAlbums, getArtists };
