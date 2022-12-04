4f608ac3587612b55ed4650f9e649721c686e01a
Asa Adomatis <90803033+AsaAdomatis@users.noreply.github.com>
2022-12-04 18:44:51 -0500

    Updated FloridaMan.js to return json

--- FloridaMan.js
+++ FloridaMan.js
@@ -1,12 +1,16 @@
 const axios = require("axios");
 
 
-async function getSearches(i, month, day) {
+const searchResults = {
+    data:  ["url1", "url2", "url3"]
+};
+
+async function getSearches(date) {
     // defining request
     const options = {
         method: 'GET',
         url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI',
-        params: {q: 'florida man ' + month + ' ' + day, pageNumber: '1', pageSize: '10', autoCorrect: 'false'},
+        params: {q: 'florida man ' + date, pageNumber: '1', pageSize: '10', autoCorrect: 'false'},
         headers: {
             'X-RapidAPI-Key': 'bf5c1ebaa5msh882d068e31c56ecp1a13dajsnb7d4ba49e04c',
             'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
@@ -18,7 +22,12 @@ async function getSearches(i, month, day) {
     // recieved request
     .then(function (response) {
         // console.log(response.data);
-        console.log(response.data.value[i].url);
+        // return response.data.value[i].url;
+        for (let i = 0; i < 3; i ++) {
+            searchResults.data[i] = response.data.value[i].url;
+        }
+        return searchResults;
+        
     })
     // request not received
     .catch(function (error) {
@@ -28,4 +37,4 @@ async function getSearches(i, month, day) {
     
 }
 
-getSearches();
+//getSearches();
