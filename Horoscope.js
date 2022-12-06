7a78f1aa8aa180e8e5b5fa66c3660e96aff1fe55
BartoszRdzanek <74029477+BartoszRdzanek@users.noreply.github.com>
2022-12-06 09:52:39 -0500

    Update Horoscope.js

--- Horoscope.js
+++ Horoscope.js
@@ -17,16 +17,16 @@ function getSign(date){
     month = +split_date[1]
     day = +split_date[2]
     //all months have less than 31 days, not going to make it month specific
-    if((month == 3 && (21 <= day <= 31)) || (month == 4 && (1 <= day <= 20))) return "Aries"
-    if((month == 4 && (21 <= day <= 31)) || (month == 5 && (1 <= day <= 20))) return "Taurus"
-    if((month == 5 && (21 <= day <= 31)) || (month == 6 && (1 <= day <= 21))) return "Gemini"
-    if((month == 6 && (22 <= day <= 31)) || (month == 7 && (1 <= day <= 22))) return "Cancer"
-    if((month == 7 && (23 <= day <= 31)) || (month == 8 && (1 <= day <= 22))) return "Leo"
-    if((month == 8 && (23 <= day <= 31)) || (month == 9 && (1 <= day <= 22))) return "Virgo"
-    if((month == 9 && (23 <= day <= 31)) || (month == 10 && (1 <= day <= 22))) return "Libra"
-    if((month == 10 && (23 <= day <= 31)) || (month == 11 && (1 <= day <=22))) return "Scorpio"
-    if((month == 11 && (23 <= day <= 31)) || (month == 12 && (1 <= day <= 21))) return "Sagittarius"
-    if((month == 12 && (12 <= day <= 31)) || (month == 1 && (1 <= day <= 19))) return "Capricorn"
-    if((month == 1 && (20 <= day <= 31)) || (month == 2 && (1 <= day <= 18))) return "Aquarius"
-    if((month == 2 && (19 <= day <= 31)) || (month == 3 && (1 <= day <= 20))) return "Pisces"
+    if((month == 3 && (21 <= day)) || (month == 4 && (1 <= day && day <= 20))) return "Aries"
+    if((month == 4 && (21 <= day)) || (month == 5 && (1 <= day && day <= 20))) return "Taurus"
+    if((month == 5 && (21 <= day)) || (month == 6 && (1 <= day && day <= 21))) return "Gemini"
+    if((month == 6 && (22 <= day)) || (month == 7 && (1 <= day && day <= 22))) return "Cancer"
+    if((month == 7 && (23 <= day)) || (month == 8 && (1 <= day && day <= 22))) return "Leo"
+    if((month == 8 && (23 <= day)) || (month == 9 && (1 <= day && day <= 22))) return "Virgo"
+    if((month == 9 && (23 <= day)) || (month == 10 && (1 <= day && day <= 22))) return "Libra"
+    if((month == 10 && (23 <= day)) || (month == 11 && (1 <= day && day <=22))) return "Scorpio"
+    if((month == 11 && (23 <= day)) || (month == 12 && (1 <= day && day <= 21))) return "Sagittarius"
+    if((month == 12 && (12 <= day)) || (month == 1 && (1 <= day && day <= 19))) return "Capricorn"
+    if((month == 1 && (20 <= day)) || (month == 2 && (1 <= day && day <= 18))) return "Aquarius"
+    if((month == 2 && (19 <= day)) || (month == 3 && (1 <= day && day <= 20))) return "Pisces"
 }
